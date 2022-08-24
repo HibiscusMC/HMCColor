6a04c35de111e1f961f4281eb6736c81c1fe5156
Boy <sivertpaulsen2@gmail.com>
2022-08-24 10:06:01 +0200

    initial commit

---
+++ src/main/java/com/hibiscusmc/hmccolor/HMCColor.kt
@@ -0,0 +1,13 @@
+package com.hibiscusmc.hmccolor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HMCColor : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
