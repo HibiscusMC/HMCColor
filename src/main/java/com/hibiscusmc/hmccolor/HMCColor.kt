a430a2a93a743abb8cb26271db5c4b8061681cad
Boy <sivertpaulsen2@gmail.com>
2022-08-24 10:24:51 +0200

    setup gradle files and other basics

--- src/main/java/com/hibiscusmc/hmccolor/HMCColor.kt
+++ src/main/java/com/hibiscusmc/hmccolor/HMCColor.kt
@@ -1,13 +1,16 @@
 package com.hibiscusmc.hmccolor
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
+val hmcColor = Bukkit.getPluginManager().getPlugin("HMCColor") as JavaPlugin
 class HMCColor : JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
+        hmcColor.server.pluginManager.registerEvents(HMCColorListeners(), hmcColor)
+        hmcColor.server.getPluginCommand("hmccolor")?.setExecutor(HMCColorCommands())
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+
     }
 }
---
+++ src/main/java/com/hibiscusmc/hmccolor/HMCColorListeners.kt
@@ -0,0 +1,13 @@
+package com.hibiscusmc.hmccolor
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerChatEvent
+
+class HMCColorListeners : Listener {
+
+    @EventHandler
+    fun PlayerChatEvent.onPlayerChat() {
+
+    }
+}
