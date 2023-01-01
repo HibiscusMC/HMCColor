e8a136aedf00473cb68f5201d968c35ce8e4d062
Boy <sivertpaulsen2@gmail.com>
2023-01-01 20:00:53 +0100

    crucible support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.hibiscusmc"
-version = "0.1.3"
+version = "0.3-SNAPSHOT"
 description = "Write something here idk.\n"
 
 
@@ -15,21 +15,24 @@ repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://repo.mineinabyss.com/releases")
+    maven("https://mvn.lumine.io/repository/maven-public/")
     maven("https://jitpack.io")
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("com.github.oraxen:oraxen:-SNAPSHOT")
     compileOnly("com.github.LoneDev6:api-itemsadder:3.2.5")
+    compileOnly("io.lumine:Mythic-Dist:5.2.0-SNAPSHOT")
+    compileOnly("io.lumine:MythicCrucible:1.6.0-SNAPSHOT")
     compileOnly("com.mineinabyss:idofront:0.12.111")
     compileOnly("com.mineinabyss:geary-papermc-core:0.19.113")
     compileOnly("com.mineinabyss:looty:0.8.67")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("net.kyori:adventure-api:4.11.0")
-    implementation("net.kyori:adventure-text-minimessage:4.11.0")
-    implementation("dev.triumphteam:triumph-gui:3.1.3")
+    implementation("net.kyori:adventure-api:4.12.0")
+    implementation("net.kyori:adventure-text-minimessage:4.12.0")
+    implementation("dev.triumphteam:triumph-gui:3.1.4")
     implementation("me.mattstudios.utils:matt-framework:1.4.6")
 }
 
