3a27f603924aab8a731e62a15d2075ad492dc706
Boy <sivertpaulsen2@gmail.com>
2023-06-13 14:26:16 +0200

    remove adventure platform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id ("org.jetbrains.kotlin.jvm") version "1.8.22"
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.mia.kotlin.jvm)
 }
@@ -25,16 +24,11 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
     compileOnly("com.github.oraxen:oraxen:1.157.0")
-    compileOnly("com.github.LoneDev6:api-itemsadder:3.4.1-r4")
+    compileOnly("com.github.LoneDev6:api-itemsadder:3.4.1e")
     compileOnly("io.lumine:Mythic-Dist:5.2.0-SNAPSHOT")
     compileOnly("io.lumine:MythicCrucible:1.6.0-SNAPSHOT")
     compileOnly("com.mineinabyss:geary-papermc:0.24-SNAPSHOT")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("net.kyori:adventure-api:4.14.0")
-    implementation("net.kyori:adventure-text-minimessage:4.14.0")
-    implementation("net.kyori:adventure-text-serializer-plain:4.14.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
     implementation("dev.triumphteam:triumph-gui:3.1.5")
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.serialization.kaml)
@@ -54,7 +48,6 @@ tasks {
             expand(mapOf("version" to pluginVersion))
         }
         relocate("dev.triumphteam.gui", "com.hibiscusmc.hmccolor.shaded.gui")
-        relocate("net.kyori:adventure-api", "com.hibiscusmc.hmccolor.shaded.kyori:adventure-api")
         relocate("org.spongepowered.configurate", "com.hibiscusmc.hmccolor.shaded.configurate")
         relocate("org.bstats", "com.hibiscusmc.hmccolor.shaded.bstats")
         relocate("kotlin", "com.hibiscusmc.hmccolor.shaded.kotlin")
