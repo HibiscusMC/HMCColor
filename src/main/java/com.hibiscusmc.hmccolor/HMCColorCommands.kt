3790f150eacbd9e26f1b8860b31a17fd2aa36fb7
Boy0000 <sivertpaulsen2@gmail.com>
2025-10-17 02:30:40 +0200

    fix: player-argument in dye-command

--- src/main/java/com.hibiscusmc.hmccolor/HMCColorCommands.kt
+++ src/main/java/com.hibiscusmc.hmccolor/HMCColorCommands.kt
@@ -2,6 +2,7 @@ package com.hibiscusmc.hmccolor
 
 import io.papermc.paper.command.brigadier.BasicCommand
 import io.papermc.paper.command.brigadier.CommandSourceStack
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 class HMCColorCommands : BasicCommand {
@@ -12,12 +13,19 @@ class HMCColorCommands : BasicCommand {
                 hmcColor.plugin.createColorContext()
                 source.sender.sendRichMessage("<green>HMCColor configs have been reloaded!")
             }
-            "dye" -> HMCColorApi.openColorMenu(source.executor as Player)
+            "dye" ->  {
+                val player = args.getOrNull(1)?.let(Bukkit::getPlayer) ?: source.sender as? Player ?: return
+                HMCColorApi.openColorMenu(player)
+            }
         }
     }
 
     override fun suggest(source: CommandSourceStack, args: Array<out String>): Collection<String> {
         if (args.isEmpty()) return emptyList()
-        return listOf("reload", "dye").filter { it.startsWith(args[0], true) }
+        return when (args.size) {
+            1 -> listOf("reload", "dye").filter { it.startsWith(args[0], true) }
+            2 if args[0] == "dye" -> Bukkit.getOnlinePlayers().mapNotNull { p -> p.name.takeIf { it.startsWith(args[0], true) } }
+            else -> emptyList()
+        }
     }
 }
